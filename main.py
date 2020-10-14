6972b4702e88c857c62b4be61df5e84cecc5ecf1
Victor Cazaes <67488598+VictorCazaes@users.noreply.github.com>
2020-10-14 17:49:21 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,3 @@
+from prime import prime
+num = int(input("Check if the number is prime or not: "))
+prime(num)  
---
+++ prime.py
@@ -0,0 +1,14 @@
+def primo(num):
+    if num > 1 and not isDivisible(num):
+        print(str(num) + " is prime")
+    else:
+        print(str(num) + " isn't prime")
+
+
+def isDivisible(num):
+    for i in range(2, num):
+        if (num % i) == 0:
+
+            return True
+
+    return False
